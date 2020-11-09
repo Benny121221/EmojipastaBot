924142529f30708a0f9e2fe12a333cd7b5615d4d
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-11-09 09:27:33 -0700

    Complies with ratelimit

--- src/emojipasta/bot.py
+++ src/emojipasta/bot.py
@@ -26,38 +26,52 @@ def main():
 	while True:
 		try:
 			for comment in user.comments.hot(limit = None):
-				skip = False
-				
-				if comment.submission.locked:
-					continue
-				
-				
-				comment.refresh()
-				if comment.parent().author == reddit.config.username:
-					continue
-
-				for reply in comment.replies:
-					if reply.author == reddit.config.username:
+				try:
+					skip = False
+					
+					if comment.submission.locked:
 						skip = True
-						break
-				
-				if not skip:
-					reply_body = gen.generate_emojipasta(comment.body)
 					
-					if len(reply_body) > 10000:
-						continue
+					if not skip:
+						comment.refresh()
+						if comment.parent().author == reddit.config.username:
+							skip = True
 					
-					comment.reply(reply_body)
+					if not skip:
+						for reply in comment.replies:
+							if reply.author == reddit.config.username:
+								skip = True
+								break
 					
-					print_header('-', time.strftime("%Y-%m-%d %H:%M:%S GMT", time.gmtime()) + '\nReply Body:\n' + reply_body)
-					#time.sleep(1)
+					if not skip:
+						reply_body = gen.generate_emojipasta(comment.body)
+						
+						if len(reply_body) > 10000:
+							continue
+						
+						comment.reply(reply_body)
+						
+						print_header('-', time.strftime("%Y-%m-%d %H:%M:%S GMT", time.gmtime()) + '\nReply Body:\n' + reply_body)
+						#time.sleep(1)
+				except BaseException as e:
+					handle_error(e)
 			
 			print_header('*', "Reached end of iterable, retrying shortly.")
 		
 		except BaseException as e:
-			print_error(e)
+			handle_error(e)
+
 		
 		#time.sleep(3)
+
+def handle_error(e):
+	print_error(e)
+	if type(e) == praw.exceptions.RedditAPIException:
+		for subexception in e.items:
+			if "RATELIMIT" in subexception.error_message:
+				print_header('*', "Sleeping for 15 minutes to comply with rate limit")
+				time.sleep(900)
+				break
 
 def print_error(error):
 	print('#' * 80)
